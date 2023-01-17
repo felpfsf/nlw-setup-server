600030dd6907e29c69d49803fc9006855fb4d925
Felipe <feferreira.sf@gmail.com>
2023-01-17 18:00:35 -0300

    refac: removed unused imports

--- src/routes/habits.routes.ts
+++ src/routes/habits.routes.ts
@@ -1,6 +1,5 @@
 import { FastifyInstance } from 'fastify'
 import { getHabits } from '../controllers/habits.controllers'
-import { prisma } from '../lib/prisma'
 
 export async function habitsRoutes(server: FastifyInstance) {
   // Get All Habits
