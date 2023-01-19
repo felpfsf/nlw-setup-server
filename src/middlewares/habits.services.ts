fb2e7b0d6de5aca225ce02699025433212182490
Felipe <feferreira.sf@gmail.com>
2023-01-19 20:15:20 -0300

    feat: habit services create data

--- src/middlewares/habits.services.ts
+++ src/middlewares/habits.services.ts
@@ -1,10 +1,35 @@
+import dayjs from 'dayjs'
 import { prisma } from '../lib/prisma'
+import { HabitBody } from '../models/habits.models'
+
+export async function createHabit(input: HabitBody) {
+  const { title, weekDays } = input
+
+  // método startOf('day')
+  const today = dayjs().startOf('day').toDate()
+
+  const habit = await prisma.habit.create({
+    data: {
+      title: title,
+      created_at: today,
+      weekDays: {
+        create: weekDays.map(weekDay => {
+          return {
+            week_day: weekDay
+          }
+        })
+      }
+    }
+  })
+
+  return habit
+}
 
 export async function findHabits() {
   return await prisma.habit.findMany({
     select: {
       title: true,
-      createdAt: true
+      created_at: true
     }
   })
 }
