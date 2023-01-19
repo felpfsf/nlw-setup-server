1afea816aaf12eb438fc73b71aa4a9275d51b691
Felipe <feferreira.sf@gmail.com>
2023-01-19 20:15:56 -0300

    feat: post habit route

--- src/routes/habits.routes.ts
+++ src/routes/habits.routes.ts
@@ -1,7 +1,21 @@
 import { FastifyInstance } from 'fastify'
-import { getHabits } from '../controllers/habits.controllers'
+import {
+  getHabits,
+  postHabitsController
+} from '../controllers/habits.controllers'
+import { habitBody } from '../models/habits.models'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
 
 export async function habitsRoutes(server: FastifyInstance) {
   // Get All Habits
   server.get('/', getHabits)
+  // Create habit
+  server.withTypeProvider<ZodTypeProvider>().route({
+    method: 'POST',
+    url: '/post',
+    schema: {
+      body: habitBody
+    },
+    handler: postHabitsController
+  })
 }
