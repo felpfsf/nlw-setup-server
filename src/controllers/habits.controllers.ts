32dd2f6260f60aa26358b68fe08ffe5a089ee5e1
Felipe <feferreira.sf@gmail.com>
2023-01-19 20:14:46 -0300

    feat: habit controller to post new habit

--- src/controllers/habits.controllers.ts
+++ src/controllers/habits.controllers.ts
@@ -1,11 +1,39 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
-import { findHabits } from '../middlewares/habits.services'
+import { ZodError } from 'zod'
+import { createHabit, findHabits } from '../middlewares/habits.services'
+import { HabitBody } from '../models/habits.models'
 
 export async function getHabits(request: FastifyRequest, reply: FastifyReply) {
   const habits = await findHabits()
-  
+
   return reply.status(200).send({
     message: 'Estes são os habitos registrados: ',
     habits
   })
+}
+
+export async function postHabitsController(
+  request: FastifyRequest<{ Body: HabitBody }>,
+  reply: FastifyReply
+) {
+  const body = request.body
+
+  try {
+    const habit = await createHabit(body)
+    return reply.status(201).send({
+      message: 'Habito criado com sucesso!',
+      habit
+    })
+  } catch (err) {
+    if (err instanceof ZodError) {
+      return reply.status(500).send({
+        message: 'Erro de validação',
+        error: JSON.parse(err.message)
+      })
+    } else if (err instanceof Error) {
+      return reply.status(500).send({
+        message: err.message
+      })
+    }
+  }
 }
